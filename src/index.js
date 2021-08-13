bbb2bb0bf1d0fddd5b7e87791724865644790e07
Junhyung So <junhyungso5551@gmail.com>
2021-08-13 16:05:33 -0400

    basename change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Router >
+  <Router basename={process.env.PUBLIC_URL}>
       <App />
   </Router>,
   document.getElementById('root')
